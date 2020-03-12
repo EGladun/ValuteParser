75413fa3073abc45f0ff0ac25db4d0a51c9f063d
Egor <gladunov.e.i.1.17@gmail.com>
2020-03-12 13:56:25 +0300

    initial commit

--- Parser/Models/TakenJSON.swift
+++ Parser/Models/TakenJSON.swift
@@ -1,25 +1,9 @@
 //
-//  TakenJSON.swift
+//  TakenJson.swift
 //  Parser
 //
-//  Created by Egor on 10.03.2020.
+//  Created by Egor on 12.03.2020.
 //  Copyright © 2020 Egor. All rights reserved.
 //
 
 import Foundation
-import Foundation
-import ObjectMapper
-
-class TakenJSON: Mappable{
-    var bpi: [String: Valute]? = [:]
-    var chartName: String? = ""
-    var disclaimer: String? = ""
-    
-    required init?(map: Map) {}
-    
-    func mapping(map: Map) {
-        bpi <- map["bpi"]
-        chartName <- map["chartName"]
-        disclaimer <- map["disclaimer"]
-    }
-}
